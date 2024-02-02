7163c5e747451db689103cac2ae95fdbbb2f12ba
gefenbar <gefenbar23@gmail.com>
2024-02-02 17:55:16 +0200

    more visual and functional changes

--- src/App.js
+++ src/App.js
@@ -29,17 +29,24 @@ Transforming landscapes with precision. Get your free estimate today!" />
       </Helmet>
       {/* Hero Section */}
       <section className="hero" id="hero">
-        <div className="image-container">
+        <div className="hero-container">
           <img className="hero-image" src="2.jpeg"/>
         </div>
         <div className="hero-overlay">
           <div className='hero-content'>
           <h1 className="hero-title">Kilauea Solutions Inc.</h1>
+        
+          </div>
+          <br/>
+          <div>
           <p className="hero-subtitle">
             Transforming landscapes with precision. Get your free estimate today!
           </p>
           </div>
+          <br/>
+          <div>
           <button className="cta-button"><a href='#contact'>Get Quote</a></button>
+          </div>
 
         </div>
       </section>
--- src/Transformation.js
+++ src/Transformation.js
@@ -3,13 +3,25 @@ import React from 'react';
 const TransformationSection = () => {
   return (
     <section className="transformation" id="transformation">
-      <h2 className="section-title">Our Transformation</h2>
+      <h2 className="section-title animate-fade-in">Witness the Transformation</h2>
       <div className="before-after">
-        <div className="before-after-container">
-          <img src="before.jpeg" alt="Before Mowing" className="before-image" />
-          <img src="after.jpeg" alt="After Mowing" className="after-image" />
+        <div className="before-after-container animate-slide-in">
+          <div className="image-container">
+            <div className="overlay">
+              <p className="overlay-text">Before</p>
+            </div>
+            <img src="before.jpeg" alt="Before Mowing" className="before-image" />
+          </div>
+          <div className="image-container">
+            <div className="overlay">
+              <p className="overlay-text">After</p>
+            </div>
+            <img src="after.jpeg" alt="After Mowing" className="after-image" />
+          </div>
         </div>
-        <p>Witness the dramatic difference our service makes on the toughest terrains.</p>
+        <p className="animate-slide-in">
+          Explore the stunning transformation our service brings to the toughest terrains.
+        </p>
       </div>
     </section>
   );
