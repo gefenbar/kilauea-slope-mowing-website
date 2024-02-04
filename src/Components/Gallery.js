ac7f728e6d0fccdb5da17fce74574a9cd949df62
gefenbar <gefenbar23@gmail.com>
2024-02-04 17:35:32 +0200

    video replacement

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -5,8 +5,8 @@ export default function Gallery() {
       <div className="image-grid">
         <img src="2.jpeg" alt="mower1" className="gallery-image" />
         <video src="mower_1.mp4" muted autoPlay className="gallery-image" />
-        {/* <video src="mower_2.mp4" muted autoPlay className="gallery-image" /> */}
-        <video src="mower_3.mp4" muted autoPlay className="gallery-image" />
+        <video src="mower_2.mp4" muted autoPlay className="gallery-image" />
+        {/* <video src="mower_3.mp4" muted autoPlay className="gallery-image" /> */}
 
         <img src="1.jpeg" alt="" className="gallery-image" />
       </div>
