0c486a86975d4043c8ee4039ac2d3706a2217745
gefenbar <gefenbar23@gmail.com>
2024-02-04 18:09:41 +0200

    fixes according lighthouse

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -4,8 +4,8 @@ export default function Gallery() {
       <h2 className="section-title">Gallery</h2>
       <div className="image-grid">
         <img src="2.jpeg" alt="mower1" className="gallery-image" />
-        <video src="mower_1.mp4" muted autoPlay className="gallery-image" />
-        <video src="mower_2.mp4" muted autoPlay className="gallery-image" />
+        <video src="mower_1.mp4" muted autoPlay loop className="gallery-image" />
+        <video src="mower_2.mp4" muted autoPlay loop className="gallery-image" />
         {/* <video src="mower_3.mp4" muted autoPlay className="gallery-image" /> */}
 
         <img src="1.jpeg" alt="" className="gallery-image" />
