b72b7e346a2edc1501b6940879b7cf3fce158059
gefenbar <gefenbar23@gmail.com>
2024-02-04 17:34:37 +0200

    design and functionaity changes

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -4,8 +4,9 @@ export default function Gallery() {
       <h2 className="section-title">Gallery</h2>
       <div className="image-grid">
         <img src="2.jpeg" alt="mower1" className="gallery-image" />
-        <video src="mower3.mp4" muted autoPlay className="gallery-image" />
-        <video src="mower2.mp4" muted autoPlay className="gallery-image" />
+        <video src="mower_1.mp4" muted autoPlay className="gallery-image" />
+        {/* <video src="mower_2.mp4" muted autoPlay className="gallery-image" /> */}
+        <video src="mower_3.mp4" muted autoPlay className="gallery-image" />
 
         <img src="1.jpeg" alt="" className="gallery-image" />
       </div>
