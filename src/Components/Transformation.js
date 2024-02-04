4186dc43c7a72a632676206e3f60ede34034c1de
gefenbar <gefenbar23@gmail.com>
2024-02-04 23:05:26 +0200

    fix console issues and package json

--- src/Components/Transformation.js
+++ src/Components/Transformation.js
@@ -11,7 +11,7 @@ export default function TransformationSection() {
               <p className="overlay-text">Before</p>
             </div>
             <img
-              src="Before.webp"
+              src="before.webp"
               alt="Before Mowing"
               className="before-image"
               loading="lazy"
@@ -24,7 +24,7 @@ export default function TransformationSection() {
               <p className="overlay-text">After</p>
             </div>
             <img
-              src="After.webp"
+              src="after.webp"
               alt="After Mowing"
               className="after-image"
               loading="lazy"
