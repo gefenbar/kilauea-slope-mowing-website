847b8dc2255138f5f2df1f3f84be75271384335c
gefenbar <gefenbar23@gmail.com>
2024-02-27 15:46:28 +0200

    Loading text for gallery - works

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from "react";
 
 export default function Gallery() {
   const [current, setCurrent] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
   const isMobile = window.innerWidth <= 768;
 
-  // Combine image and video data efficiently
   const media = [
     { src: "mower_1.webp", mobileSrc: "mower_1_mobile.webp", isVideo: false },
     { src: "mower_1.mp4", isVideo: true },
@@ -12,36 +12,32 @@ export default function Gallery() {
     { src: "mower_2.mp4", isVideo: true },
   ];
 
-  // Preload next and previous images/videos based on media type and mobile state
   useEffect(() => {
+    setIsLoading(true);
     const nextIndex = (current + 1) % media.length;
-    const prevIndex = (current - 1 + media.length) % media.length;
-    const nextMedia = isMobile
-      ? media[nextIndex].mobileSrc
-      : media[nextIndex].src;
-    const prevMedia = isMobile
-      ? media[prevIndex].mobileSrc
-      : media[prevIndex].src;
+    const nextMedia = isMobile ? media[nextIndex].mobileSrc : media[nextIndex].src;
 
-    const preloadImage = (url) => {
-      const img = new Image();
-      img.src = url;
+    const preloadMedia = (url, isVideo) => {
+      if (isVideo) {
+        const video = document.createElement("video");
+        video.onloadeddata = () => setIsLoading(false);
+        video.src = url;
+      } else {
+        const img = new Image();
+        img.onload = () => setIsLoading(false);
+        img.src = url;
+      }
     };
 
-    preloadImage(nextMedia);
-    preloadImage(prevMedia);
+    preloadMedia(nextMedia, media[nextIndex].isVideo);
   }, [current, isMobile, media]);
 
   const handleNext = () => {
-    setCurrent((prevCurrent) =>
-      prevCurrent === media.length - 1 ? 0 : prevCurrent + 1
-    );
+    setCurrent((prevCurrent) => (prevCurrent === media.length - 1 ? 0 : prevCurrent + 1));
   };
 
   const handlePrev = () => {
-    setCurrent((prevCurrent) =>
-      prevCurrent === 0 ? media.length - 1 : prevCurrent - 1
-    );
+    setCurrent((prevCurrent) => (prevCurrent === 0 ? media.length - 1 : prevCurrent - 1));
   };
 
   return (
@@ -51,22 +47,12 @@ export default function Gallery() {
         <button onClick={handleNext} className="nav-button next-button">
           {" >"}
         </button>
-        <div>
+        <div className="image-wrapper">
+          {isLoading && <div className="loading-text">Loading...</div>}
           {media[current].isVideo ? (
-            <video
-              src={media[current].src}
-              alt="slope mowing, mowing video"
-              muted
-              autoPlay
-              loop
-              className="gallery-image"
-            />
+            <video src={media[current].src} alt="slope mowing, mowing video" muted autoPlay loop className="gallery-image" />
           ) : (
-            <img
-              src={isMobile ? media[current].mobileSrc : media[current].src}
-              alt="slope mowing, mower image"
-              className="gallery-image"
-            />
+            <img src={isMobile ? media[current].mobileSrc : media[current].src} alt="slope mowing, mower image" className="gallery-image" />
           )}
         </div>
         <button onClick={handlePrev} className="nav-button prev-button">
