46fd2a279a98616acd885f900db3378598811bb6
gefenbar <gefenbar23@gmail.com>
2024-05-29 22:58:46 +0300

    small fix

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -2,41 +2,60 @@ import React, { useState, useEffect } from "react";
 
 export default function Gallery() {
   const [current, setCurrent] = useState(0);
-  const [isLoading, setIsLoading] = useState(true); // Set initial loading state to true
+  const [isLoading, setIsLoading] = useState(false);
   const isMobile = window.innerWidth <= 768;
 
   const media = [
     { src: "mower_1.webp", mobileSrc: "mower_1_mobile.webp", isVideo: false },
-    { src: "mower_2.mp4", isVideo: true },
+    { src: "mower_1.mp4", isVideo: true },
     { src: "mower_2.webp", mobileSrc: "mower_2_mobile.webp", isVideo: false },
     { src: "mower_2.mp4", isVideo: true },
   ];
-
   useEffect(() => {
-    const preloadedImages = media.map((item) => {
-      const preloadMedia = (url) => {
-        return new Promise((resolve) => {
-          const mediaElement = item.isVideo ? document.createElement("video") : new Image();
-          mediaElement.onload = () => {
-            resolve();
+    setIsLoading(true);
+    const nextIndex = (current + 1) % media.length;
+    const nextMedia = isMobile
+      ? media[nextIndex].mobileSrc
+      : media[nextIndex].src;
+  
+    if (nextMedia) {
+      const preloadMedia = (url, isVideo) => {
+        if (isVideo) {
+          const video = document.createElement("video");
+          video.onloadeddata = () => {
+            setIsLoading(false);
+            document.body.removeChild(video); // Remove the video element after loading
           };
-          mediaElement.src = url;
-        });
+          video.src = url;
+          video.style.display = "none"; // Hide the video element
+          document.body.appendChild(video); // Append the video to start loading
+        } else {
+          const img = new Image();
+          img.onload = () => {
+            setIsLoading(false);
+            document.body.removeChild(img); // Remove the image element after loading
+          };
+          img.src = url;
+          img.style.display = "none"; // Hide the image element
+          document.body.appendChild(img); // Append the image to start loading
+        }
       };
-      return preloadMedia(isMobile ? item.mobileSrc : item.src);
-    });
-
-    Promise.all(preloadedImages).then(() => {
-      setIsLoading(false); // Set loading to false once all media is preloaded
-    });
-  }, [isMobile, media]);
+  
+      preloadMedia(nextMedia, media[nextIndex].isVideo);
+    }
+  }, [current, isMobile, media]);
+  
 
   const handleNext = () => {
-    setCurrent((prevCurrent) => (prevCurrent === media.length - 1 ? 0 : prevCurrent + 1));
+    setCurrent((prevCurrent) =>
+      prevCurrent === media.length - 1 ? 0 : prevCurrent + 1
+    );
   };
 
   const handlePrev = () => {
-    setCurrent((prevCurrent) => (prevCurrent === 0 ? media.length - 1 : prevCurrent - 1));
+    setCurrent((prevCurrent) =>
+      prevCurrent === 0 ? media.length - 1 : prevCurrent - 1
+    );
   };
 
   return (
