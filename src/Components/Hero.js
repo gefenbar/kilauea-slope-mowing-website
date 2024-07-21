8bd72d0495f5eb2c3bbc7e7efad0d56a01fa6a5f
gefenbar <gefenbar23@gmail.com>
2024-07-21 12:54:21 +0300

    removed unused comments

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,20 +1,6 @@
 export default function Hero() {
   return (
     <section className="hero" id="hero">
-        {/* <img
-          className="hero-image"
-          src="mower_1.webp"
-          alt="mower"
-          width="0px"
-          height="0px"
-        /> */}
-         {/* <img
-          className="hero-image-mobile"
-          src="mower_1_mobile.webp"
-          alt="mower"
-          width="100%"
-          height="auto"
-        /> */}
       <div className="hero-overlay">
         <div className="hero-content">
           <h1 className="hero-title">Kilauea Solutions Inc.</h1>
--- src/Components/SocialLinks.js
+++ src/Components/SocialLinks.js
@@ -8,20 +8,6 @@ export default function SocialLinks({ isBackgroundGreen }) {
       >
         <img src={"email.webp"} alt="Email" />
       </a>
-      {/* <a
-        href="tel:xxx"
-        className={`social-link ${isBackgroundGreen ? "bright-outline" : ""}`}
-      >
-        <img src={"phone.webp"} alt="Phone" />
-      </a>
-      <a
-        href="https://wa.me/xxx"
-        target="_blank"
-        rel="noopener noreferrer"
-        className={`social-link ${isBackgroundGreen ? "bright-outline" : ""}`}
-      >
-        <img src={"whatsapp.webp"} alt="WhatsApp" />
-      </a> */}
     </div>
   );
 }
